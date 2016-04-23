bd9ba386a1485e1cc974f86b80cd8939afa464cd
silentknight <abhijit65@gmail.com>
2016-04-23 19:22:27 +0530

    Added scipy, numpy, scikit-learn

--- birdCLEFmain.py
+++ birdCLEFmain.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
-import pyaudio
-import wave
+import time
+import scipy
+import scipy.io.wavfile as sciwav
+import matplotlib.pyplot as plt
+import numpy
 import os
 import xml.dom.minidom
 
@@ -21,29 +24,13 @@ def access_all_data():
 			filename_wav = os.path.join(wavPath, tempfile)
 			filename_xml = os.path.join(xmlPath, os.path.splitext(tempfile)[0]+".xml")
 
-			# Display XML
 			xmlfile = xml.dom.minidom.parse(filename_xml)
-			pretty_xml_as_string = xmlfile.toprettyxml()
-			print pretty_xml_as_string
 
-			#Play Audio
-			CHUNK = 1024
-			wf = wave.open(filename_wav, 'rb')
-			p = pyaudio.PyAudio()
-			
-			stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-				channels=wf.getnchannels(),
-				rate=wf.getframerate(),
-				output=True)
-			data = wf.readframes(CHUNK)
-			while len(data) > 0:
-				stream.write(data)
-				data = wf.readframes(CHUNK)
+			[rate, data] = sciwav.read(filename_wav)
+			plt.plot(data)
+			plt.show()
 
-			stream.stop_stream()
-			stream.close()
-			p.terminate()
-
+			time.sleep(1)
 
 if __name__ == "__main__":
 	try:
